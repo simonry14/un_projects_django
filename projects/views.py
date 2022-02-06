8c2cdd8b44dbadc88c2cbe966e67a6b7192561a2
Simon Peter Miyingo <simonpetermiyingo@gmail.com>
2022-02-06 19:13:31 +0300

    mob

--- projects/views.py
+++ projects/views.py
@@ -16,27 +16,33 @@ def home(request):
     context = {'projects': projects}
     return render(request, 'projects/home.html', context)
 
+
 def add(request):
     form = ProjectForm()
     if request.method=="POST":
-        form = ProjectForm(request.post)
+        form = ProjectForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
-        
-    
-    context={'form': form}
+    context={'form': form ,'title': 'Add Project'}
     return render(request, 'projects/add.html', context)
 
 def view(request,id):
     proje = project.objects.get(id=id)
-    context = {'proje': proje}
+    context = {'project': proje}
     return render(request, 'projects/view.html', context)
 
 
 def edit(request, id):
+    pro = project.objects.get(id = id)
+    form = ProjectForm(instance=pro)
+    context = {'form': form, 'title': 'Edit Project'}
+    if request.method=="POST":
+        form = ProjectForm(request.POST, instance=pro)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'projects/add.html', context)
     
-    return render(request, 'projects/edit.html',)
-    #return HttpResponse("Edit Project")
 
 # Create your views here.
